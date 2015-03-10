d9933e9da0cd0d8e7063e4a434521bb0e3c84080
rurouni-hou-ou <connorblakey96@gmail.com>
2015-03-10 12:56:47 +0000

    Added defaut constructor and renamed class and file.

---
+++ ChessGame/ChessGame/GamePiece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame
+{
+    /// <summary>
+    /// Base class for all chess pieces.
+    /// </summary>
+    class GamePiece
+    {
+        public GamePiece()
+        {
+
+        }
+    }
+}
