9a0b3b11d127984d43015fa53bd6f83b7a90db88
rurouni-hou-ou <connorblakey96@gmail.com>
2015-03-13 13:50:01 +0000

    Added licences to top of files.

--- ChessGame/ChessGame/MainGame.cs
+++ ChessGame/ChessGame/MainGame.cs
@@ -1,3 +1,21 @@
+/*   MainGame.cs - Main file of game operation.
+*    Copyright (C) 2015  Connor Blakey <connorblakey96@gmail.com>, Matthew Burling <mattyburlin@gmail.com>.
+*
+*    This program is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License along
+*    with this program; if not, write to the Free Software Foundation, Inc.,
+*    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,12 +33,13 @@ namespace ChessGame
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class MainGame : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         private MouseState oldMouseState;
-        public Game1()
+        
+        public MainGame()
         {
             // Graphics allocation + resolution 
             graphics = new GraphicsDeviceManager(this);
@@ -101,7 +120,6 @@ namespace ChessGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
             base.Draw(gameTime);
         }
     }
